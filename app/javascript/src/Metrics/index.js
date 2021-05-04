6b7f3bceef8d486fa84a2b3d97ab555d9a707a38
josemigallas <josemigallas@gmail.com>
2021-05-04 17:55:12 +0200

    Scaffolding new Metrics index page for products

---
+++ app/javascript/src/Metrics/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+export * from './components/IndexPage'
+export * from './components/ProductIndexPage'
+export * from './components/MethodsTable'
+// $FlowFixMe[cannot-resolve-module] module mapper messing up cause of there's a 'Metrics' module
+export * from './components/MetricsTable'
